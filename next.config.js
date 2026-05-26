daf030db252c8108dbe66fa91568241ab07e40ed
thapthepevo1995 <thapthepevo1995@gmail.com>
2026-05-26 23:06:33 +0700

    fix next.config.js remove invalid experimental.runtime

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
-/** @type {import('next').NextConfig} */
+/** @type {import(`"next"`).NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'rycjohhdgvmwzpdqhtrk.supabase.co',
-        pathname: '/storage/v1/object/public/**',
+        protocol: `"https"`,
+        hostname: `"rycjohhdgvmwzpdqhtrk.supabase.co"`,
+        pathname: `"/storage/v1/object/public/**"`,
       },
     ],
-  },
-  experimental: {
-    runtime: 'edge',
   },
 }
 
