0693075b35ad716344dd5301dc5fbfc21cd0c16e
thapthepevo1995 <thapthepevo@gmail.com>
2026-05-26 20:07:25 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'rycjohhdgvmwzpdqhtrk.supabase.co',
-        pathname: '/storage/v1/object/public/**',
-      },
-    ],
+  experimental: {
+    runtime: 'edge',
   },
 }
 
